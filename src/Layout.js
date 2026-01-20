cacb2ca8a213ce7b7fc85c90035a4bf4bf1b6ed8
KyLeo25 <vertusarmageddon88@gmail.com>
2026-01-20 12:49:43 +0700

    fav

--- src/Layout.js
+++ src/Layout.js
@@ -2,20 +2,6 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar.jsx";
 import Footer from "../components/Footer.jsx";
 
-// function Layout() {
-//   return (
-//     <div className="app">
-//       <Navbar />
-
-//       <main className="main-content">
-//         <Outlet />
-//       </main>
-
-//       <Footer />
-//     </div>
-//   );
-// }
-
 function Layout() {
   return (
     <div className="app">
