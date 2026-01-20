122c0fd2f8973bd9d28d204a426e7d97304ce9b7
KyLeo25 <vertusarmageddon88@gmail.com>
2026-01-20 12:12:14 +0700

    japan update

--- src/pages/japan/HomeJapan.jsx
+++ src/pages/japan/HomeJapan.jsx
@@ -44,17 +44,49 @@ export default function Home() {
       to : "/japan/osaka"
     }
   ];
+  const addToFavorite = () => {
+    const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+    const japan = {
+      id: "Japan",
+      title: "Japan",
+      image: tokyo,
+      link: "/japan",
+    };
+
+    const exists = favorites.find((item) => item.id === japan.id);
+    if (exists) {
+      alert("Japan sudah ada di Favorite ❤️");
+      return;
+    }
+
+    favorites.push(japan);
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+    alert("Japan ditambahkan ke Favorite ❤️");
+  };
 
   return (
-    <div className={styles['main-bg']}>
+    <div className={styles["main-bg"]}>
       <div className="container mt-5" id="wiljep">
-        <div className="container d-flex justify-content-between pt-5" id="wiljep1">
+        <div
+          className="container d-flex justify-content-between pt-5"
+          id="wiljep1"
+        >
           <Link to="/list-ticket">
             <button className="btn" id="button-back">
               🔙 Back
             </button>
           </Link>
-          <h1 className="text-center text-danger fw-bold">Explore Japan - City Tours</h1>
+          <button
+            onClick={addToFavorite}
+            className="btn btn-danger"
+            id="fav"
+          >
+            ❤️ Favorite
+          </button>
+          <h1 className="text-center text-danger fw-bold">
+            Explore Japan - City Tours
+          </h1>
           <ul className="navbar-nav  mb-2 mb-lg-0 d-flex align-items-center gap-2">
             <li className="nav-item">
               <CurrencySelector />
@@ -62,13 +94,10 @@ export default function Home() {
           </ul>
         </div>
         <div className="row g-4 mt-4">
-          {
-            destinations.map((destination, index) => (
-              <MainCard key={index} dest={destination} />
-            ))
-          }
+          {destinations.map((destination, index) => (
+            <MainCard key={index} dest={destination} />
+          ))}
         </div>
-        
       </div>
     </div>
   );
